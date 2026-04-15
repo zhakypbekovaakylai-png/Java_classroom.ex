cfcfbd6805af81712020c706408600d8ce8f5a54
zhakypbekovaakylai-png <zhakypbekovaakylai@gmail.com>
2026-04-15 09:54:39 +0200

    Update SillySum.java

--- Lesson1/SillySum.java
+++ Lesson1/SillySum.java
@@ -1,1 +1,18 @@
+public class SillySum {
+    public static void main(String[] args) {
+        System.out.print("32+4= ");     //it prints string , no new line
+        System.out.println(doIt(32,4));    //prints the value returned by method doIt
+    }
+
+    public static int  doIt (int x, int y){
+        int result = x;
+
+        while (y>0) {
+            result++;
+            y--;
+        }
+
+        return result;
+    }
+}
 
